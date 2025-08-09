3b82f4e90426fc10f5442d9de7abda24de12a9d7
Suhayb Ali <suhayb10910@gmail.com>
2025-08-09 16:45:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const header = document.getElementById('header');
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) {
-    header.classList.add('shrink');
-  } else {
-    header.classList.remove('shrink');
-  }
-});
